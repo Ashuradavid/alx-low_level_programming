2602a0c67c0c21e8d1583683270f4761d937f1dd
Ashuradavid <solomondavidfreestar@gmail.com>
2022-10-31 19:00:14 -0700

    tasku

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,32 @@
+/*
+ * File: 0-memset.c
+ * Auth: not
+ */
+
 #include "main.h"
 
 /**
- * _memset - fill memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to fill
- * @n: byte of memory area to fill
+ * _memset - Fills the first n bytes of the memory area
+ * pointed to by @s with the constant byte @c
+ * @s: A pointer to the memory area to be filled.
+ * @b: constant byte
+ * @n: The number of bytes to be filled.
+ * description _memset: over there
  *
- * Return: the memory area filled
+ * Return: A pointer to the filled memory area @s.
+ *
  */
 char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int a;
 
-	for (a = 0; < n; a++)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
 	{
-		s[a] = b;
+		*(s + i) = b; /*add 1 position s*/
+
 	}
 
 	return (s);
-	}
+}
